02946b8861c04e0c086a35dee0db296346ddae32
weichao <342597109@qq.com>
2020-11-25 22:54:16 +0800

    服务器变身卡文件修改

--- gs/confsrc/fire/pb/item/STransformationEffectConfig.java
+++ gs/confsrc/fire/pb/item/STransformationEffectConfig.java
@@ -1,13 +1,16 @@
 package fire.pb.item;
 
 
-public class STransformationEffectConfig implements mytools.ConvMain.Checkable ,Comparable<STransformationEffectConfig>{
+public class STransformationEffectConfig  extends TransformationEffectConfig {
 
 	public int compareTo(STransformationEffectConfig o){
 		return this.id-o.id;
 	}
 
 	
+	public STransformationEffectConfig(TransformationEffectConfig arg){
+		super(arg);
+	}
 	
 	static class NeedId extends RuntimeException{
 
@@ -21,14 +24,24 @@ public class STransformationEffectConfig implements mytools.ConvMain.Checkable ,
 		super();
 	}
 	public STransformationEffectConfig(STransformationEffectConfig arg){
+		super(arg);
 		this.id=arg.id ;
 		this.shapeid=arg.shapeid ;
 		this.color=arg.color ;
-		this.skills=arg.skills ;
-		this.skillfactors=arg.skillfactors ;
-		this.skillconstants=arg.skillconstants ;
+		this.speed_value=arg.speed_value ;
+		this.uplimithp_value=arg.uplimithp_value ;
+		this.curmp_value=arg.curmp_value ;
+		this.magicdef_value=arg.magicdef_value ;
+		this.magicattack_value=arg.magicattack_value ;
+		this.phyattack_value=arg.phyattack_value ;
+		this.defend_value=arg.defend_value ;
+		this.magicdouble_value=arg.magicdouble_value ;
+		this.medical_value=arg.medical_value ;
+		this.phycruellv_value=arg.phycruellv_value ;
+		this.magiccruellv_value=arg.magiccruellv_value ;
 	}
 	public void checkValid(java.util.Map<String,java.util.Map<Integer,? extends Object> > objs){
+			super.checkValid(objs);
 	}
 	/**
 	 * id
@@ -72,40 +85,144 @@ public class STransformationEffectConfig implements mytools.ConvMain.Checkable ,
 	/**
 	 * 
 	 */
-	public java.util.ArrayList<Integer> skills  ;
+	public int speed_value  = 0  ;
 	
-	public java.util.ArrayList<Integer> getSkills(){
-		return this.skills;
+	public int getSpeed_value(){
+		return this.speed_value;
 	}
 	
-	public void setSkills(java.util.ArrayList<Integer> v){
-		this.skills=v;
-	}
-	
-	/**
-	 * 
-	 */
-	public java.util.ArrayList<Integer> skillfactors  ;
-	
-	public java.util.ArrayList<Integer> getSkillfactors(){
-		return this.skillfactors;
-	}
-	
-	public void setSkillfactors(java.util.ArrayList<Integer> v){
-		this.skillfactors=v;
+	public void setSpeed_value(int v){
+		this.speed_value=v;
 	}
 	
 	/**
 	 * 
 	 */
-	public java.util.ArrayList<Integer> skillconstants  ;
+	public int uplimithp_value  = 0  ;
 	
-	public java.util.ArrayList<Integer> getSkillconstants(){
-		return this.skillconstants;
+	public int getUplimithp_value(){
+		return this.uplimithp_value;
 	}
 	
-	public void setSkillconstants(java.util.ArrayList<Integer> v){
-		this.skillconstants=v;
+	public void setUplimithp_value(int v){
+		this.uplimithp_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int curmp_value  = 0  ;
+	
+	public int getCurmp_value(){
+		return this.curmp_value;
+	}
+	
+	public void setCurmp_value(int v){
+		this.curmp_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int magicdef_value  = 0  ;
+	
+	public int getMagicdef_value(){
+		return this.magicdef_value;
+	}
+	
+	public void setMagicdef_value(int v){
+		this.magicdef_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int magicattack_value  = 0  ;
+	
+	public int getMagicattack_value(){
+		return this.magicattack_value;
+	}
+	
+	public void setMagicattack_value(int v){
+		this.magicattack_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int phyattack_value  = 0  ;
+	
+	public int getPhyattack_value(){
+		return this.phyattack_value;
+	}
+	
+	public void setPhyattack_value(int v){
+		this.phyattack_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int defend_value  = 0  ;
+	
+	public int getDefend_value(){
+		return this.defend_value;
+	}
+	
+	public void setDefend_value(int v){
+		this.defend_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int magicdouble_value  = 0  ;
+	
+	public int getMagicdouble_value(){
+		return this.magicdouble_value;
+	}
+	
+	public void setMagicdouble_value(int v){
+		this.magicdouble_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int medical_value  = 0  ;
+	
+	public int getMedical_value(){
+		return this.medical_value;
+	}
+	
+	public void setMedical_value(int v){
+		this.medical_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int phycruellv_value  = 0  ;
+	
+	public int getPhycruellv_value(){
+		return this.phycruellv_value;
+	}
+	
+	public void setPhycruellv_value(int v){
+		this.phycruellv_value=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int magiccruellv_value  = 0  ;
+	
+	public int getMagiccruellv_value(){
+		return this.magiccruellv_value;
+	}
+	
+	public void setMagiccruellv_value(int v){
+		this.magiccruellv_value=v;
 	}
 	
 	
