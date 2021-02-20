6ba7de38d593d6db3187ce2e85f4c4f38899da65
weichao <342597109@qq.com>
2021-02-20 13:12:24 +0800

    增加宠物穿戴装备消息字段

--- game_server/gs/src/fire/pb/item/CPetPutOnEquip.java
+++ game_server/gs/src/fire/pb/item/CPetPutOnEquip.java
@@ -28,20 +28,23 @@ public class CPetPutOnEquip extends __CPetPutOnEquip__ {
 	}
 
 	public int pet_packkey;
+	public int pet_oldpackkey;
 	public int pet_dstpos;
 	public int petkey;
 
 	public CPetPutOnEquip() {
 	}
 
-	public CPetPutOnEquip(int _pet_packkey_, int _pet_dstpos_, int _petkey_) {
+	public CPetPutOnEquip(int _pet_packkey_, int _pet_oldpackkey_, int _pet_dstpos_, int _petkey_) {
 		this.pet_packkey = _pet_packkey_;
+		this.pet_oldpackkey = _pet_oldpackkey_;
 		this.pet_dstpos = _pet_dstpos_;
 		this.petkey = _petkey_;
 	}
 
 	public final boolean _validator_() {
 		if (pet_packkey < 1) return false;
+		if (pet_oldpackkey < 1) return false;
 		if (pet_dstpos < 0) return false;
 		if (petkey < 1) return false;
 		return true;
@@ -52,6 +55,7 @@ public class CPetPutOnEquip extends __CPetPutOnEquip__ {
 			throw new VerifyError("validator failed");
 		}
 		_os_.marshal(pet_packkey);
+		_os_.marshal(pet_oldpackkey);
 		_os_.marshal(pet_dstpos);
 		_os_.marshal(petkey);
 		return _os_;
@@ -59,6 +63,7 @@ public class CPetPutOnEquip extends __CPetPutOnEquip__ {
 
 	public OctetsStream unmarshal(OctetsStream _os_) throws MarshalException {
 		pet_packkey = _os_.unmarshal_int();
+		pet_oldpackkey = _os_.unmarshal_int();
 		pet_dstpos = _os_.unmarshal_int();
 		petkey = _os_.unmarshal_int();
 		if (!_validator_()) {
@@ -72,6 +77,7 @@ public class CPetPutOnEquip extends __CPetPutOnEquip__ {
 		if (_o1_ instanceof CPetPutOnEquip) {
 			CPetPutOnEquip _o_ = (CPetPutOnEquip)_o1_;
 			if (pet_packkey != _o_.pet_packkey) return false;
+			if (pet_oldpackkey != _o_.pet_oldpackkey) return false;
 			if (pet_dstpos != _o_.pet_dstpos) return false;
 			if (petkey != _o_.petkey) return false;
 			return true;
@@ -82,6 +88,7 @@ public class CPetPutOnEquip extends __CPetPutOnEquip__ {
 	public int hashCode() {
 		int _h_ = 0;
 		_h_ += pet_packkey;
+		_h_ += pet_oldpackkey;
 		_h_ += pet_dstpos;
 		_h_ += petkey;
 		return _h_;
@@ -91,6 +98,7 @@ public class CPetPutOnEquip extends __CPetPutOnEquip__ {
 		StringBuilder _sb_ = new StringBuilder();
 		_sb_.append("(");
 		_sb_.append(pet_packkey).append(",");
+		_sb_.append(pet_oldpackkey).append(",");
 		_sb_.append(pet_dstpos).append(",");
 		_sb_.append(petkey).append(",");
 		_sb_.append(")");
@@ -101,6 +109,8 @@ public class CPetPutOnEquip extends __CPetPutOnEquip__ {
 		if (_o_ == this) return 0;
 		int _c_ = 0;
 		_c_ = pet_packkey - _o_.pet_packkey;
+		if (0 != _c_) return _c_;
+		_c_ = pet_oldpackkey - _o_.pet_oldpackkey;
 		if (0 != _c_) return _c_;
 		_c_ = pet_dstpos - _o_.pet_dstpos;
 		if (0 != _c_) return _c_;
