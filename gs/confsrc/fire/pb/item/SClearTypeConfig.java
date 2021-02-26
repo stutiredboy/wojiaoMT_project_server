5b60ca4dabcb97f59bcf0795a409c9ad5004a5cd
weichao <342597109>
2021-02-26 17:58:08 +0800

    增加清理类型表

---
+++ gs/confsrc/fire/pb/item/SClearTypeConfig.java
@@ -0,0 +1,56 @@
+package fire.pb.item;
+
+
+public class SClearTypeConfig implements mytools.ConvMain.Checkable ,Comparable<SClearTypeConfig>{
+
+	public int compareTo(SClearTypeConfig o){
+		return this.id-o.id;
+	}
+
+	
+	
+	static class NeedId extends RuntimeException{
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		
+	}
+	public SClearTypeConfig(){
+		super();
+	}
+	public SClearTypeConfig(SClearTypeConfig arg){
+		this.id=arg.id ;
+		this.clearType=arg.clearType ;
+	}
+	public void checkValid(java.util.Map<String,java.util.Map<Integer,? extends Object> > objs){
+	}
+	/**
+	 * 
+	 */
+	public int id  = 0  ;
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public void setId(int v){
+		this.id=v;
+	}
+	
+	/**
+	 * 
+	 */
+	public int clearType  = 0  ;
+	
+	public int getClearType(){
+		return this.clearType;
+	}
+	
+	public void setClearType(int v){
+		this.clearType=v;
+	}
+	
+	
+};
